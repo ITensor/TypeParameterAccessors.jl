c56284792ba9e41c3df9f0bcec90ccdbea364972
Matt Fishman <mtfishman@users.noreply.github.com>
2024-11-25 21:13:28 -0500

    Update package configuration (#4)

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,15 @@
-using TypeParameterAccessors
-using Documenter
+using TypeParameterAccessors: TypeParameterAccessors
+using Documenter: Documenter, DocMeta, deploydocs, makedocs
 
 DocMeta.setdocmeta!(
   TypeParameterAccessors, :DocTestSetup, :(using TypeParameterAccessors); recursive=true
 )
 
+include("make_index.jl")
+
 makedocs(;
   modules=[TypeParameterAccessors],
-  authors="ITensor developers",
+  authors="ITensor developers <support@itensor.org> and contributors",
   sitename="TypeParameterAccessors.jl",
   format=Documenter.HTML(;
     canonical="https://ITensor.github.io/TypeParameterAccessors.jl",
@@ -15,6 +17,10 @@ makedocs(;
     assets=String[],
   ),
   pages=["Home" => "index.md"],
+  # TODO: Define missing docs and delete this.
+  warnonly=[:missing_docs],
 )
 
-deploydocs(; repo="github.com/ITensor/TypeParameterAccessors.jl", devbranch="main")
+deploydocs(;
+  repo="github.com/ITensor/TypeParameterAccessors.jl", devbranch="main", push_preview=true
+)
