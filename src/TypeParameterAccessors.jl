7a3c98204acbbba1494b7857147ad10225f548db
Lukas Devos <ldevos98@gmail.com>
2024-12-07 07:51:43 -0500

    cleanup unused files

--- src/TypeParameterAccessors.jl
+++ src/TypeParameterAccessors.jl
@@ -1,20 +1,7 @@
 module TypeParameterAccessors
 
 include("type_utils.jl")
-# Interface
-# include("interface.jl")
-# include("to_unionall.jl")
-# include("parameters.jl")
-# include("abstractposition.jl")
-# include("abstracttypeparameter.jl")
 include("type_parameters.jl")
-# include("position.jl")
-# include("parameter.jl")
-# include("is_parameter_specified.jl")
-# include("unspecify_parameters.jl")
-# include("set_parameters.jl")
-# include("specify_parameters.jl")
-# include("default_parameters.jl")
 
 # Implementations
 include("ndims.jl")
