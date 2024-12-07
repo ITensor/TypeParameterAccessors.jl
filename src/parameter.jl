56656061d8a5e97bae83dcabb28e38f3111427ba
Lukas Devos <ldevos98@gmail.com>
2024-12-07 07:51:43 -0500

    cleanup unused files

--- src/parameter.jl
+++
@@ -1,3 +0,0 @@
-@inline parameter(type::Type, pos) = parameter(type, position(type, pos))
-@inline parameter(type::Type, pos::Position) = parameters(type)[Int(pos)]
-@inline parameter(type::Type) = only(parameters(type))
