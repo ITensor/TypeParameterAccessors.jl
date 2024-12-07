0644fcba9d0d280af8c2cf8571e0f04361860dc9
Lukas Devos <ldevos98@gmail.com>
2024-12-06 19:24:52 -0500

    cleanup tests
    cleanup test_defaults
    
    cleanup test_customtypes
    
    cleanup type_parameters

--- test/test_defaults.jl
+++ test/test_defaults.jl
@@ -8,63 +8,52 @@ using TypeParameterAccessors:
   set_default_type_parameters,
   specify_default_type_parameter,
   specify_default_type_parameters
-include("utils/test_inferred.jl")
+using TestExtras: @constinferred
 @testset "TypeParameterAccessors defaults" begin
   @testset "Erroneously requires wrapping to infer" begin end
   @testset "Get defaults" begin
-    @test_inferred default_type_parameter(Array, 1) == Float64 wrapped = true
-    @test_inferred default_type_parameter(Array, Position(1)) == Float64
-    @test_inferred default_type_parameter(Array, 2) == 1 wrapped = true
-    @test_inferred default_type_parameter(Array, Position(2)) == 1
-    @test_inferred default_type_parameters(Array) == (Float64, 1)
-    @test_inferred default_type_parameters(Array, 2, 1) == (1, Float64) wrapped = true
-    @test_inferred default_type_parameters(Array, Position(2), Position(1)) == (1, Float64)
-    @test_inferred default_type_parameters(Array, ndims, eltype) == (1, Float64)
+    @test @constinferred(default_type_parameter($Array, 1)) == Float64
+    @test @constinferred(default_type_parameter($Array, 2)) == 1
+    @test @constinferred(default_type_parameters($Array)) == (Float64, 1)
+    @test @constinferred(default_type_parameters($Array, 2, 1)) == (1, Float64)
+    @test @constinferred(default_type_parameters($Array, ndims, eltype)) == (1, Float64)
   end
+
   @testset "Set defaults" begin
-    @test_inferred set_default_type_parameter(Array{Float32}, 1) == Array{Float64} wrapped =
-      true
-    @test_inferred set_default_type_parameter(Array{Float32}, Position(1)) == Array{Float64}
-    @test_inferred set_default_type_parameter(Array{Float32}, eltype) == Array{Float64}
-    @test_inferred set_default_type_parameters(Array{Float32}) == Vector{Float64}
-    @test_inferred set_default_type_parameters(Array{Float32}, 1, 2) == Vector{Float64} wrapped =
-      true
-    @test_inferred set_default_type_parameters(Array{Float32}, Position(1), Position(2)) ==
+    @test @constinferred(set_default_type_parameter($(Array{Float32}), 1)) == Array{Float64}
+    @test @constinferred(set_default_type_parameter($(Array{Float32}), eltype)) ==
+      Array{Float64}
+    @test @constinferred(set_default_type_parameters($(Array{Float32}))) == Vector{Float64}
+    @test @constinferred(set_default_type_parameters($(Array{Float32}), 1, 2)) ==
       Vector{Float64}
-    @test_inferred set_default_type_parameters(Array{Float32}, eltype, ndims) ==
+    @test @constinferred(set_default_type_parameters($(Array{Float32}), eltype, ndims)) ==
       Vector{Float64}
-    @test_inferred set_default_type_parameters(Array) == Vector{Float64} wrapped = true
-    @test_inferred set_default_type_parameters(Array, Position(1)) == Array{Float64}
-    @test_inferred set_default_type_parameters(Array, Position(1), Position(2)) ==
-      Vector{Float64}
+    @test @constinferred(set_default_type_parameters($Array)) == Vector{Float64}
+    @test @constinferred(set_default_type_parameters($Array, 1)) == Array{Float64}
+    @test @constinferred(set_default_type_parameters($Array, 1, 2)) == Vector{Float64}
   end
+
   @testset "Specify defaults" begin
-    @test_inferred specify_default_type_parameter(Array, 1) == Array{Float64} wrapped = true
-    @test_inferred specify_default_type_parameter(Array, Position(1)) == Array{Float64}
-    @test_inferred specify_default_type_parameter(Array, eltype) == Array{Float64}
-    @test_inferred specify_default_type_parameter(Array, 2) == Vector wrapped = true
-    @test_inferred specify_default_type_parameter(Array, Position(2)) == Vector
-    @test_inferred specify_default_type_parameter(Array, ndims) == Vector
-    @test_inferred specify_default_type_parameters(Array) == Vector{Float64}
-    @test_inferred specify_default_type_parameters(Array, 1) == Array{Float64} wrapped =
-      true
-    @test_inferred specify_default_type_parameters(Array, Position(1)) == Array{Float64}
-    @test_inferred specify_default_type_parameters(Array, eltype) == Array{Float64}
-    @test_inferred specify_default_type_parameters(Array, 2) == Vector wrapped = true
-    @test_inferred specify_default_type_parameters(Array, Position(2)) == Vector
-    @test_inferred specify_default_type_parameters(Array, ndims) == Vector
-    @test_inferred specify_default_type_parameters(Array, 1, 2) == Vector{Float64} wrapped =
-      true
-    @test_inferred specify_default_type_parameters(Array, Position(1), Position(2)) ==
+    @test @constinferred(specify_default_type_parameter($Array, 1)) == Array{Float64}
+    @test @constinferred(specify_default_type_parameter($Array, eltype)) == Array{Float64}
+    @test @constinferred(specify_default_type_parameter($Array, 2)) == Vector
+    @test @constinferred(specify_default_type_parameter($Array, ndims)) == Vector
+    @test @constinferred(specify_default_type_parameters($Array)) == Vector{Float64}
+    @test @constinferred(specify_default_type_parameters($Array, 1)) == Array{Float64}
+    @test @constinferred(specify_default_type_parameters($Array, eltype)) == Array{Float64}
+    @test @constinferred(specify_default_type_parameters($Array, 2)) == Vector
+    @test @constinferred(specify_default_type_parameters($Array, ndims)) == Vector
+    @test @constinferred(specify_default_type_parameters($Array, 1, 2)) == Vector{Float64}
+    @test @constinferred(specify_default_type_parameters($Array, eltype, ndims)) ==
       Vector{Float64}
-    @test_inferred specify_default_type_parameters(Array, eltype, ndims) == Vector{Float64}
   end
+
   @testset "On objects" begin
     a = randn(Float32, (2, 2, 2))
-    @test_inferred default_type_parameter(a, 1) == Float64 wrapped = true
-    @test_inferred default_type_parameter(a, eltype) == Float64
-    @test_inferred default_type_parameter(a, 2) == 1 wrapped = true
-    @test_inferred default_type_parameter(a, ndims) == 1
-    @test_inferred default_type_parameters(a) == (Float64, 1)
+    @test @constinferred(default_type_parameter(a, 1)) == Float64
+    @test @constinferred(default_type_parameter(a, eltype)) == Float64
+    @test @constinferred(default_type_parameter(a, 2)) == 1
+    @test @constinferred(default_type_parameter(a, ndims)) == 1
+    @test @constinferred(default_type_parameters(a)) == (Float64, 1)
   end
 end
