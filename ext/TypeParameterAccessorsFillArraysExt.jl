d86918df9c5e22d86c9fa91f8da4099f0a7171d8
Lukas Devos <ldevos98@gmail.com>
2025-01-06 13:52:39 -0500

    Code style suggestion

--- ext/TypeParameterAccessorsFillArraysExt.jl
+++ ext/TypeParameterAccessorsFillArraysExt.jl
@@ -4,10 +4,12 @@ using TypeParameterAccessors: TypeParameterAccessors, Position
 using FillArrays: Fill, Zeros, Ones
 
 for T in (:Fill, :Zeros, :Ones)
-  @eval TypeParameterAccessors.position(::Type{<:$T}, ::typeof(eltype)) = Position(1)
-  @eval TypeParameterAccessors.position(::Type{<:$T}, ::typeof(ndims)) = Position(2)
-  @eval TypeParameterAccessors.position(::Type{<:$T}, ::typeof(axes)) = Position(3)
-  @eval TypeParameterAccessors.default_type_parameters(::Type{<:$T}) = (Float64, 0, Tuple{})
+  @eval begin
+    TypeParameterAccessors.position(::Type{<:$T}, ::typeof(eltype)) = Position(1)
+    TypeParameterAccessors.position(::Type{<:$T}, ::typeof(ndims)) = Position(2)
+    TypeParameterAccessors.position(::Type{<:$T}, ::typeof(axes)) = Position(3)
+    TypeParameterAccessors.default_type_parameters(::Type{<:$T}) = (Float64, 0, Tuple{})
+  end
 end
 
 end
